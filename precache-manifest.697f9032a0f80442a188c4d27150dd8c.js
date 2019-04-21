49317144ec7a31c6e910f8c74ce75c623e85cb9a
hacknaman <hacknaman@gmail.com>
2019-04-21 16:55:13 +0530

    test react build

---
+++ precache-manifest.697f9032a0f80442a188c4d27150dd8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Snake-Game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5628087a6e48f1752c06",
+    "url": "/React-Snake-Game/static/js/runtime~main.efa06e0f.js"
+  },
+  {
+    "revision": "d334d7f58a95c5c60803",
+    "url": "/React-Snake-Game/static/js/main.b02e5773.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/React-Snake-Game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "d334d7f58a95c5c60803",
+    "url": "/React-Snake-Game/static/css/main.0c9309a4.chunk.css"
+  },
+  {
+    "revision": "25e3eb35df6434bba3866cbda1fb3bc9",
+    "url": "/React-Snake-Game/index.html"
+  }
+];
