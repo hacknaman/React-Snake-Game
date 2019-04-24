cbdbf8b27795ab6751bd355c5deb2d0e325e5c06
hacknaman <hacknaman@gmail.com>
2019-04-24 23:19:35 +0530

    made grid for snake game

--- project/src/App.js
+++ project/src/App.js
@@ -1,25 +1,65 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+// display a single cell
+function GridCell(props) {
+  return (
+    <div
+      className={`grid-cell`}
+      style={{ height: props.size + "px", width: props.size + "px" }}
+      />
+  );
+}
 
 class App extends Component {
+  constructor(props) {
+
+    super(props);
+    this.state = {
+      snake: [],
+      food: [],
+      // 0 = not started, 1 = in progress, 2= finished
+      status: 0,
+      // using keycodes to indicate direction
+      direction: 39
+    };
+
+  }
+
   render() {
+    // each cell should be approximately 15px wide, so calculate how many we need
+    this.numCells = Math.floor(this.props.size / 15);
+    const cellSize = this.props.size / this.numCells;
+    const cellIndexes = Array.from(Array(this.numCells).keys());
+    const cells = cellIndexes.map(y => {
+      return cellIndexes.map(x => {
+
+        return (
+          <GridCell size={cellSize} key={x + " " + y} />
+        );
+      });
+    });
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-              My snake game goes here...
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div
+        className="snake-board"
+        style={{
+          width: this.props.size + "px",
+          height: this.props.size + "px"
+        }}
+        ref={el => (this.el = el)}
+        tabIndex={-1}
+        >
+        <div
+          className="grid"
+          style={{
+            width: this.props.size + "px",
+            height: this.props.size + "px"
+          }}
           >
-            Learn React
-          </a>
-        </header>
+          {cells}
+        </div>
       </div>
     );
   }
